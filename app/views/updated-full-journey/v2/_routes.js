d87f96ed6bea364e6efcaee8db9fce47728af318
M-bell07 <megan.bell@nhsbsa.nhs.uk>
2023-08-10 17:20:53 +0100

    sorted out radio buttons for where you collect and continue screens

--- app/views/updated-full-journey/v2/_routes.js
+++ app/views/updated-full-journey/v2/_routes.js
@@ -4,6 +4,40 @@ const router = express.Router();
 
 
 //Updated Full Journey
+
+//continue
+router.post(/where-you-collect/, (req, res) => {
+
+  const UserLocation= req.session.data["prescriptionCountryName"]
+
+  if (UserLocation == "england") {
+    res.redirect('date-of-birth');
+
+  } else if (UserLocation== "scotland") {
+    res.redirect('continue')
+
+  } else if (UserLocation== "wales") {
+    res.redirect('continue')
+
+  } else if (UserLocation== "northern") {
+    res.redirect('continue')
+  }
+})
+
+
+//continue
+router.post(/continue/,  function (req, res) {
+  // creating a variable named contact - assigning the variable the value of the radio button selected
+  var TestContinue = req.session.data["continue"]
+  if (TestContinue == "yes"){
+    res.redirect('date-of-birth')
+  }
+  else {
+    res.redirect('chosen-not-to-buy')
+  }
+})
+
+
 //fulfilment
 router.post(/fulfilment/,  function (req, res) {
   // creating a variable named contact - assigning the variable the value of the radio button selected
