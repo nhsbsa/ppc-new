75dabf71edcd5415b3a7a164a8935e4bc8310e33
Chukwudi Chima <chchi@nhsbsa.nhs.uk>
2025-03-25 13:11:06 +0000

    price updates made in v7 and v8 pages

---
+++ app/data/session-data-defaults.js
@@ -0,0 +1,19 @@
+//
+// DATA
+//
+
+module.exports = {
+
+    NHSPrescriptionCost: '£10.15',
+
+    HRTPPCCost: '£20.30',
+
+    HRTPPCandNHSPrescriptionCost: '£30.45',
+
+    PPC3MonthsCost: '£32.80',
+
+    PPC12MonthsCost: '£117.30',
+
+    PPCDirectDebitCost: '£11.73'
+
+}
